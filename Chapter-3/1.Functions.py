cf58d957a9b4ba797f5b264342aeaee20d08d670
pawan9489 <pawankumar.gubbala@gmail.com>
2018-11-18 18:44:44 +0530

    Function

---
+++ Chapter-3/1.Functions.py
@@ -0,0 +1,34 @@
+'''
+Syntax: 
+    def  function_name(input_parameters) :
+        Code With or Without return
+'''
+
+# Defining a function
+def func():
+    print('Python')
+
+# Calling a function
+func()
+
+print()
+# Passing parameters
+def fun_params(lang):
+    print(lang + ' Language')
+
+fun_params('Python')
+
+print()
+# Default Parameters
+
+print()
+# Keyword Parameters
+
+print()
+# Default Parameters
+
+print()
+# Default Parameters
+
+print()
+# Default Parameters
